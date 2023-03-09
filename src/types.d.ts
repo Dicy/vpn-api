93352974e9f3385f77e187928703dd0e2854d831
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-09 23:40:36 +0100

    Add SendGrid envs

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,6 +3,9 @@ import type {Env as HonoEnv} from "hono";
 export interface Env extends Record<string, unknown> {
   DATA_API_URL: string; // MongoDB Data API URL
   DATA_API_KEY: string; // MongoDB Data API Key
+  SENDGRID_API_URL: string;
+  SENDGRID_API_KEY: string;
+  SENDGRID_FROM_EMAIL: string; // the email address to send emails from
 }
 
 declare global {
