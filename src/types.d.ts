7216c02da9a2270c5992c7c57ebe6581f1aa0220
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-10 00:46:36 +0100

    Use token secret to generate JWT

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,7 @@
 import type {Env as HonoEnv} from "hono";
 
 export interface Env extends Record<string, unknown> {
+  TOKEN_SECRET: string; // the secret used to sign JWTs
   DATA_API_URL: string; // MongoDB Data API URL
   DATA_API_KEY: string; // MongoDB Data API Key
   SENDGRID_API_URL: string;
