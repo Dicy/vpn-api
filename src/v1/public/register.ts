394da63310c87474f02d7ee35b714913087ec557
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-10 23:28:31 +0100

    Add endpoint to verify email address

--- src/v1/public/register.ts
+++ src/v1/public/register.ts
@@ -5,7 +5,7 @@ import jwt from "@tsndr/cloudflare-worker-jwt";
 
 import { Env } from "../../types";
 import { accounts, pendingAccounts } from "../../database";
-import { obfuscate } from "../../utils/stringObfuscation";
+import { deobfuscate, obfuscate } from "../../utils/stringObfuscation";
 import { Reply } from "../reply";
 import { hashPassword } from "../../utils/passwords";
 import PermissionLevel from "../../permissionLevel";
@@ -44,10 +44,11 @@ register.post("/register", zValidator("json", z.object({
     return Reply.serverError("Failed to create account");
   }
 
+  console.info("Account created, insertId:", response.insertedId);
+
   // send verification email
   const verificationToken = await jwt.sign({
-    // @ts-ignore
-    accountId: response.insertedId.$oid,
+    accountId: response.insertedId,
     exp: Math.floor(Date.now() / 1000) + VERIFICATION_EXPIRATION
   }, c.env.TOKEN_SECRET);
 
@@ -62,6 +63,62 @@ register.post("/register", zValidator("json", z.object({
   return Reply.ok("Account created and verification email sent");
 });
 
+register.post("/verify-email", zValidator("json", z.object({
+  verificationToken: z.string().min(1).max(10_000)
+})), async (c) => {
+  const { verificationToken } = c.req.valid("json");
+  const isValid: boolean = await jwt.verify(verificationToken, c.env.TOKEN_SECRET);
+  if (!isValid) {
+    return Reply.unauthorized("Invalid verification token");
+  }
+
+  const { payload: { accountId } } = jwt.decode(verificationToken);
+  // get the pending account from the database
+  const response = await pendingAccounts(c.env).findOne({
+    filter: { _id: { $oid: accountId } }
+  });
+  if (!response || !response.document) {
+    return Reply.serverError("Failed to find account, please try again");
+  }
+  console.info("Deleting pending account with id:", accountId);
+  // delete the pending account
+  await pendingAccounts(c.env).deleteOne({
+    filter: { _id: { $oid: accountId } }
+  });
+
+  const email = deobfuscate(response.document.email);
+  console.info("Creating account with email:", email);
+
+  // check if the email is already in use (another account was created in the meantime)
+  if (!await isEmailAvailable(email, c.env)) {
+    console.info("Email is already in use, aborting account creation");
+    return Reply.conflict("This email is linked to another account");
+  }
+
+  // add the account to the accounts collection
+  console.warn("DELETE:", response.document._id);
+  delete response.document._id;
+  console.info("Inserting account:", response.document);
+  const accountResponse = await accounts(c.env).insertOne({
+    document: response.document
+  });
+  if (!accountResponse || !accountResponse.insertedId) {
+    console.error("Failed to create account:", accountResponse);
+    return Reply.serverError("Failed to verify account");
+  }
+  console.info("Account created with id:", accountResponse.insertedId);
+
+  // send welcome email
+  await sendEmail({
+    to: email,
+    type: "email-verified",
+    data: {
+      name: response.document.name
+    }
+  }, c.env);
+  return Reply.ok("Account verified");
+});
+
 export default register;
 
 
@@ -69,5 +126,5 @@ export async function isEmailAvailable(email: string, env: Env): Promise<boolean
   const response = await accounts(env).findOne({
     filter: { email: obfuscate(email.toLowerCase()) }
   });
-  return response && response.document !== null;
+  return response && response.document === null;
 }
