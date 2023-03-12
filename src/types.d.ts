b95752283d45738077b5b716166e8a3ad57ec8cb
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-12 13:57:22 +0100

    Add KV namespace for invalidated refresh tokens

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,6 +2,7 @@ import type {Env as HonoEnv} from "hono";
 
 interface Env extends Record<string, unknown> {
   TOKEN_SECRET: string; // the secret used to sign JWTs
+  INVALIDATED_TOKENS: KVNamespace; // contains IDs of invalidated refresh tokens
   DATABASE: string; // database name
   DATA_API_URL: string; // MongoDB Data API URL
   DATA_API_KEY: string; // MongoDB Data API Key
