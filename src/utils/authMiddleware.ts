6d661f85448971f4b04beb9e40c2be46cb506412
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-12 14:14:10 +0100

    Add auth middleware for non-public path

---
+++ src/utils/authMiddleware.ts
@@ -0,0 +1,72 @@
+import { HTTPException } from "hono/http-exception";
+import { Context, MiddlewareHandler } from "hono";
+import jwt from "@tsndr/cloudflare-worker-jwt";
+
+export const authMiddleware: MiddlewareHandler = async (c, next) => {
+  const credentials = c.req.headers.get("Authorization");
+
+  if (credentials) {
+    const parts = credentials.split(/\s+/);
+    if (parts.length !== 2 || parts[0].toLowerCase() !== "bearer") {
+      sendMalformedAuthHeaderError(c);
+      return;
+    }
+    if (parts[0].toLowerCase() !== "bearer") {
+      sendMalformedAuthHeaderError(c);
+      return;
+    }
+    const token = parts[1];
+    // check if the token is valid
+    try {
+      const isValid = await jwt.verify(token, c.env.TOKEN_SECRET);
+      if (!isValid) {
+        sendInvalidTokenError(c, "invalid token");
+        return;
+      }
+    } catch (e: any) {
+      sendInvalidTokenError(c, `${e}`)
+      return;
+    }
+
+    const { payload } = jwt.decode(token);
+    // check if the token is invalidated
+    const invalidated = await c.env.INVALIDATED_TOKENS.get(payload.refreshTokenId);
+    if (invalidated) {
+      sendInvalidTokenError(c, "token invalidated");
+    }
+
+    return await next();
+  }
+  throwMissingToken(c);
+};
+
+function sendMalformedAuthHeaderError(c: Context) {
+  const res = new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": `Bearer realm="${c.req.url}",error="invalid_request",error_description="invalid credentials structure"`
+    }
+  });
+  throw new HTTPException(401, { res });
+}
+
+function throwMissingToken(c: Context) {
+  const res = new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": `Bearer realm="${c.req.url}",error="invalid_request",error_description="no authorization included in request"`
+    }
+  });
+  throw new HTTPException(401, { res });
+}
+
+function sendInvalidTokenError(c: Context, message: string) {
+  const res = new Response("Unauthorized", {
+    status: 401,
+    statusText: message,
+    headers: {
+      "WWW-Authenticate": `Bearer realm="${c.req.url}",error="invalid_token",error_description="token verification failure"`
+    }
+  });
+  throw new HTTPException(401, { res });
+}
--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,8 +1,10 @@
-import {Hono} from "hono";
+import { Hono } from "hono";
+
 import register from "./public/register";
 import login from "./public/login";
 import refreshToken from "./public/refreshToken";
 import passwordReset from "./public/passwordReset";
+import { authMiddleware } from "../utils/authMiddleware";
 
 const v1 = new Hono<AppEnv>();
 
@@ -10,9 +12,9 @@ const publicRoutes = new Hono<AppEnv>();
 publicRoutes.route("/", register);
 publicRoutes.route("/", login);
 publicRoutes.route("/", passwordReset);
-// publicRoutes.route("/", resetPassword);
 publicRoutes.route("/", refreshToken);
 
 v1.route("/public", publicRoutes);
+v1.use("*", authMiddleware);
 
 export default v1;
